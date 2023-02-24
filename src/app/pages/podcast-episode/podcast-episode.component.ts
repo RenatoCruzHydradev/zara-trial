fb52821fcf0eafa8abc04a33bbda7850192a4886
Renato Cruz <renato.cruz@siliconexpert.com>
2023-02-24 12:23:53 +0000

    Allow direct navigation to episode

--- src/app/pages/podcast-episode/podcast-episode.component.ts
+++ src/app/pages/podcast-episode/podcast-episode.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { PodcastService } from 'src/app/services/podcast-service.service';
 import { StateService } from 'src/app/services/state.service';
 
 @Component({
@@ -9,16 +12,38 @@ import { StateService } from 'src/app/services/state.service';
 export class PodcastEpisodeComponent implements OnDestroy {
 
   episode: any;
-  constructor(private state: StateService) {
+  episodeId: number;
+  podcastId: number;
+  podcastSub: Subscription;
+  constructor(private state: StateService, private service: PodcastService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.episode = this.state.getEpisode();
+    if (!this.episode) {
+      this.route.params.subscribe((params: any) => {
+        this.episodeId = params.episodeId
+        this.podcastId = params.podcastId
+        this.getPodcastEpisodes();
+      });
+    }
+  }
+
+  private getPodcastEpisodes() {
+    this.podcastSub = this.service
+      .getPodcastsEpisodes(this.podcastId)
+      .subscribe((res: any) => {
+        this.episode = res.results.find((ep: any) => {
+          return ep.trackId == this.episodeId
+        })
+
+      });
   }
 
   ngOnDestroy(): void {
     this.state.clearEpisode()
+    this.podcastSub?.unsubscribe()
   }
 
 }
